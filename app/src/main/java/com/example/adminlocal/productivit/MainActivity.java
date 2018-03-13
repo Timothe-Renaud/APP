860a1386f4437af77288db97c9b4e830087a80ca
admin.local <timothe8833@gmail.com>
2018-03-13 09:11:38 +0100

    commit 1

--- app/src/main/java/com/example/adminlocal/productivit/MainActivity.java
+++ app/src/main/java/com/example/adminlocal/productivit/MainActivity.java
@@ -10,4 +10,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //ajoute les entrées de menu_test à l'ActionBar
+        getMenuInflater().inflate(R.menu.menu_test, menu);
+        return true;
 }
